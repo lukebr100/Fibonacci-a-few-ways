87dc92e50177b66d12dd9f083e2c02a19fa9bc18
Luke Ballantine Bennett <bennettluke33@yahoo.com>
2023-06-16 13:28:27 -0500

    Update times_whilefib.py

--- times_whilefib.py
+++ times_whilefib.py
@@ -1,12 +1,9 @@
 import time
+from whilefib import whilefib
 
 def times_fibwhile(n):
-  elapse = [0 for _ in range(n)]
-  for i in range(100):
-    start = time.time()
-    whilefib(i)
-    end = time.time()
-    elapse[i] = end - start
-    print(elapse[i])
-
+  start = time.time()
+  whilefib(i)
+  end = time.time()
+  elapse = end - start
   return elapse
