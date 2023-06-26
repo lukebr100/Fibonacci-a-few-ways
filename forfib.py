c66f3185414128a75290c7a2ce096783be85528c
Luke Ballantine Bennett <bennettluke33@yahoo.com>
2023-06-26 11:23:23 -0500

    Update forfib.py

--- forfib.py
+++ forfib.py
@@ -18,3 +18,16 @@ def forfib(n):
             return tildeF                           # returns the list of fib nums
     else:                                           # only executes if user inputs invalid 'n'
         print("n must be a non-negative integer") 
+
+# input: n a natural number
+# output: list of fibonacci numbers of length n + 1
+def recfib(n):
+    if natural_number(n) == True:            # executes only for non-negative integers
+        if n == 0:                                    # base case 0
+            return [0]
+        elif n == 1:                                  # base case 1
+            return recfib(0) + [1]
+        else:                                         # non-trivial cases, n >= 2
+            return  recfib(n - 1)   + [recfib(n - 1)[n - 1] + recfib(n - 2)[n - 2]]            # the previous list of fib. numbers concatenated with
+    else:                                                                                      # a list of the sum of the last entries two function values
+        print("n must be a non-negative integer")    # executes only for non-natural numbers
