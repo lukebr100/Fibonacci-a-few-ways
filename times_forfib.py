47065184c62ba24470155dffbea2578573681990
Luke Ballantine Bennett <bennettluke33@yahoo.com>
2023-06-26 11:21:44 -0500

    Update times_forfib.py

--- times_forfib.py
+++ times_forfib.py
@@ -1,10 +1,18 @@
 import time
 from forfib import forfib
+from recfib import recfib
 
 def times_forfib(n):
   start = time.time()
   forfib(n)
   end = time.time()
   elapse = end - start
-  return(elapse)
+  return elapse
+
+def times_recfib(n):
+  start = time.time()
+  recfib(n)
+  end = time.time()
+  elapse = end - start
+  return elapse
  
