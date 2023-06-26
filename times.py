f915d1afe6aae4c247abc6ba15311522628ade90
Luke Ballantine Bennett <bennettluke33@yahoo.com>
2023-06-26 11:22:28 -0500

    Update and rename times_forfib.py to times.py

--- times.py
+++ times.py
@@ -1,17 +1,26 @@
 import time
 from forfib import forfib
 from recfib import recfib
+from whilefib import whilefib
 
-def times_forfib(n):
+def fortime(n):
   start = time.time()
   forfib(n)
   end = time.time()
   elapse = end - start
   return elapse
 
-def times_recfib(n):
+def rectime(n):
   start = time.time()
   recfib(n)
+  end = time.time()
+  elapse = end - start
+  return elapse
+
+
+def whiletime(n):
+  start = time.time()
+  whilefib(i)
   end = time.time()
   elapse = end - start
   return elapse
